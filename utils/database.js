e5b507302d9bcffde3e761ec5473e2604f91946a
Mayur Wagh <mayur2002wagh@gmail.com>
2024-11-09 21:44:14 +0530

    deploy commits

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,7 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
+console.log(process.env.DATABASE_URL)
 
 const Database = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
